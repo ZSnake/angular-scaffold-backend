989648fd238fd3fe248d473475b9c5749d155b13
viktor <viktor@acklenavenue.com>
2015-12-06 03:21:38 -0600

    removed params from root

--- routes.js
+++ routes.js
@@ -2,7 +2,7 @@ var studentsController = require('./controllers/studentsController');
 var usersController = require('./controllers/usersController');
 var authController = require('./controllers/authController');
 
-exports.endpoints = [{method: 'GET', path: '/{param*}', config: {handler: function(request, reply){reply('API v1, Students')}}},
+exports.endpoints = [{method: 'GET', path: '/', config: {handler: function(request, reply){reply('API v1, Students')}}},
 	{method: 'GET', path: '/v1/students', config: studentsController.getStudents},
   {method: 'POST', path: '/v1/student', config: studentsController.createStudent},
 	{method: 'POST', path: '/v1/register', config: usersController.createUser},
